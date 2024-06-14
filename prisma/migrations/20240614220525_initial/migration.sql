d862ed2ff71f4dcb7202aff1ac65f78f53e4dd5f
Nonatto <robsonnonatoiii@gmail.com>
2024-06-14 19:31:45 -0300

    mergeado

---
+++ prisma/migrations/20240614220525_initial/migration.sql
@@ -0,0 +1,170 @@
+-- CreateEnum
+CREATE TYPE "Modalidade" AS ENUM ('PRESENCIAL', 'ONLINE');
+
+-- CreateTable
+CREATE TABLE "Usuarios" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "cargo" TEXT NOT NULL,
+    "telefone" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "accessLevel" TEXT NOT NULL DEFAULT 'administrativo',
+    "ativo" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Usuarios_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AgendaAulas" (
+    "id" TEXT NOT NULL,
+    "alunoId" TEXT NOT NULL,
+    "professorId" TEXT NOT NULL,
+    "data" TIMESTAMP(3) NOT NULL,
+    "hora" TEXT NOT NULL,
+    "local" TEXT NOT NULL,
+    "modalidade" "Modalidade" NOT NULL,
+    "finalizada" BOOLEAN NOT NULL DEFAULT false,
+    "cancelada" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "AgendaAulas_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Responsavel" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "telefone" TEXT NOT NULL,
+    "endereco" JSONB NOT NULL,
+    "password" TEXT NOT NULL,
+    "accessLevel" TEXT NOT NULL DEFAULT 'responsavel',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Responsavel_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Aluno" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "escola" TEXT NOT NULL,
+    "ano_escolar" TEXT NOT NULL,
+    "data_nascimento" TIMESTAMP(3) NOT NULL,
+    "telefone" TEXT NOT NULL,
+    "endereco" JSONB NOT NULL,
+    "ficha" TEXT NOT NULL,
+    "modalidade" JSONB NOT NULL DEFAULT '{"presencial": true, "online": true}',
+    "password" TEXT NOT NULL,
+    "responsavelId" TEXT NOT NULL,
+    "financeiro" JSONB NOT NULL,
+    "dificuldades" TEXT[],
+    "accessLevel" TEXT NOT NULL DEFAULT 'aluno',
+    "ativo" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Aluno_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Financeiro" (
+    "id" TEXT NOT NULL,
+    "alunoId" TEXT NOT NULL,
+    "qtd_aulas_compradas" INTEGER NOT NULL,
+    "qtd_aulas_executadas" INTEGER NOT NULL,
+    "valor" DOUBLE PRECISION NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'pendente',
+    "conferidoConta" BOOLEAN NOT NULL DEFAULT false,
+    "dataPagamento" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Financeiro_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Professor" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "telefone" TEXT NOT NULL,
+    "endereco" JSONB NOT NULL,
+    "areaFormacao" JSONB[],
+    "ativo" BOOLEAN NOT NULL DEFAULT true,
+    "disponibilidade" JSONB NOT NULL DEFAULT '{"segunda": {"manha": false, "tarde": true, "noite": false}, "terca": {"manha": false, "tarde": true, "noite": false}, "quarta": {"manha": false, "tarde": true, "noite": false}, "quinta": {"manha": false, "tarde": true, "noite": false}, "sexta": {"manha": false, "tarde": true, "noite": false}, "sabado": {"manha": true, "tarde": false, "noite": false}}',
+    "ficha" JSONB[],
+    "accessLevel" TEXT NOT NULL DEFAULT 'professor',
+    "modalidade" JSONB NOT NULL DEFAULT '{"presencial": true, "online": false}',
+    "password" TEXT NOT NULL,
+    "materias" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Professor_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Materias" (
+    "id" TEXT NOT NULL,
+    "materia" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Materias_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Series" (
+    "id" TEXT NOT NULL,
+    "serie" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Series_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Usuarios_email_key" ON "Usuarios"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Usuarios_cpf_key" ON "Usuarios"("cpf");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AgendaAulas_professorId_data_hora_key" ON "AgendaAulas"("professorId", "data", "hora");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Responsavel_email_key" ON "Responsavel"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Responsavel_cpf_key" ON "Responsavel"("cpf");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Aluno_email_key" ON "Aluno"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Professor_email_key" ON "Professor"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Professor_cpf_key" ON "Professor"("cpf");
+
+-- AddForeignKey
+ALTER TABLE "AgendaAulas" ADD CONSTRAINT "AgendaAulas_alunoId_fkey" FOREIGN KEY ("alunoId") REFERENCES "Aluno"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AgendaAulas" ADD CONSTRAINT "AgendaAulas_professorId_fkey" FOREIGN KEY ("professorId") REFERENCES "Professor"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Aluno" ADD CONSTRAINT "Aluno_responsavelId_fkey" FOREIGN KEY ("responsavelId") REFERENCES "Responsavel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Financeiro" ADD CONSTRAINT "Financeiro_alunoId_fkey" FOREIGN KEY ("alunoId") REFERENCES "Aluno"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
