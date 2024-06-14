5aaa0a3ef1d30887f821f0e026384b75795006f8
Robson Nonato <108809490+NonattoDev@users.noreply.github.com>
2024-06-14 11:08:59 -0300

    patch corrected

--- src/app/help/admin/factory/edit/professor/[id]/page.tsx
+++ src/app/help/admin/factory/edit/professor/[id]/page.tsx
@@ -1,4 +1,4 @@
-import EditProfessor from "@/components/EditForms/EditProfessor";
+import EditProfessor from "@/components/EditForms/EditProfessor/EditProfessor";
 import prisma from "../../../../../../../../prisma/prismaInstance";
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
