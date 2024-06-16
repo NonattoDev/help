d267d2c69a296b2637e17d9e5a1533d2fb1e856e
NonattoDev <robsonnonatoiii@gmail.com>
2024-06-16 19:39:41 -0300

    order professores on match by more close to the user desc

--- src/app/help/admin/match/components/ShowProfessores.tsx
+++ src/app/help/admin/match/components/ShowProfessores.tsx
@@ -10,16 +10,15 @@ interface ShowProfessores {
 }
 
 export default function ShowProfessores({ professores, aluno }: ShowProfessores) {
-  // Aqui será implementado a lógica de agenda
-  // Já que temos os professores que correspondem as materias e série do aluno, podemos fazer o usuário escolher uma data e horário, após isso mostrar quais professores estão disponíveis naquele horário e data.
-  // O usuário poderá escolher um professor e então será feito o match.
+  // Ordenar os professores pela distância em km (do menor para o maior)
+  const sortedProfessores = [...professores].sort((a, b) => a.distanciaEmKm - b.distanciaEmKm);
 
   return (
     <>
       <h2 className="text-center text-2xl font-semibold"> Professores que correspondem ao aluno </h2>
       <div className="flex justify-around">
         <div className="grid grid-cols-4 gap-8">
-          {professores.map((professor) => (
+          {sortedProfessores.map((professor) => (
             <div key={professor.id}>
               <CardProfessor aluno={aluno} professor={professor} />
             </div>
