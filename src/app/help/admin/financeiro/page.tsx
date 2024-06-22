b1c174c7ced2472d36e4c652bbca759867388242
NonattoDev <robsonnonatoiii@gmail.com>
2024-06-21 23:44:49 -0300

    iniciando tabela financeiro

--- src/app/help/admin/financeiro/page.tsx
+++ src/app/help/admin/financeiro/page.tsx
@@ -28,6 +28,10 @@ const getDadosFinancas = async () => {
         lte: endOfWeek,
       },
     },
+    include: {
+      aluno: true,
+      professor: true,
+    },
   });
 
   return {
@@ -45,6 +49,41 @@ export default async function FinanceiroPage() {
       <div>Existem {aulasRealizadas.length} aulas realizadas que ainda não foram pagas.</div>
       <div>Existem {aulasCanceladasComValor.length} aulas que foram canceladas após o prazo limite</div>
       <div>Existe uma previsão de pagamento de {totalAulasRealizadas.length} aulas até o final da semana</div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Aluno</th>
+              <th>Professor</th>
+              <th>Data da aula</th>
+              <th>Hora</th>
+              <th>Duração</th>
+              <th>Valor da Aula</th>
+              <th>Modalidade</th>
+              <th>Finalizada</th>
+              <th>Cancelada</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {totalAulasRealizadas.map((aula) => (
+              <tr>
+                <td>{aula.id}</td>
+                <td>{aula.aluno.nome}</td>
+                <td>{aula.professor.nome}</td>
+                <td>{moment(aula.data).format("DD/MM/YYYY")}</td>
+                <td>{aula.horaInicio}</td>
+                <td>{aula.duracao}</td>
+                <td>{aula.valor_aula}</td>
+                <td>{aula.modalidade}</td>
+                <td>{aula.finalizada ? "Sim" : "Não"}</td>
+                <td>{aula.cancelada ? "Sim" : "Não"}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
