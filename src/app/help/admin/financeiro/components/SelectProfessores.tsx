ec034f9417522e8e10661c1800677342cc9b7d68
Nonatto <robsonnonatoiii@gmail.com>
2024-06-23 16:34:10 -0300

    correct select and value of all professores

--- src/app/help/admin/financeiro/components/SelectProfessores.tsx
+++ src/app/help/admin/financeiro/components/SelectProfessores.tsx
@@ -15,21 +15,21 @@ export default function SelectProfessores({ HandleSelectChange }: SelectProfesso
   if (error) return <div>Erro ao carregar os dados.</div>;
 
   const professores: Professor[] = data.data;
-  
+
   return (
-    <>
-      <h2 className="text text-1xl font-semibold mb-2">Selecione um professor</h2>
-      <select className="input input-bordered" name="professores" id="professores" onChange={HandleSelectChange} defaultValue="">
+    <div className="flex flex-col items-center my-8">
+      <h2 className="text text-1xl font-semibold mb-2 text-center">Selecione um professor</h2>
+      <select className="input input-bordered w-1/4" name="professores" id="professores" onChange={HandleSelectChange} defaultValue="">
         <option value="" disabled>
           Selecione uma opção
         </option>
-        <option value="all">Todos os professores</option>
+        <option value="">Todos os professores</option>
         {professores.map((professor) => (
           <option key={professor.id} value={professor.id}>
             {professor.nome}
           </option>
         ))}
       </select>
-    </>
+    </div>
   );
 }
