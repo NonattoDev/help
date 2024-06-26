4f7066ae8a6d2c5b54d1ec700800c48371c4b689
Nonatto <robsonnonatoiii@gmail.com>
2024-06-26 20:24:10 -0300

    correct modal call

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,23 +1,27 @@
 import React, { useEffect } from "react";
 
 const Modal = ({ show, onClose, children }: { show: boolean; onClose: () => void; children: React.ReactNode }) => {
-  if (!show) {
-    return null;
-  }
-
-  const handleClickOutside = (event: MouseEvent) => {
-    const target = event.target as HTMLElement;
-    if (target.classList.contains("modal-overlay")) {
-      onClose();
-    }
-  };
-
   useEffect(() => {
+    if (!show) {
+      return;
+    }
+
+    const handleClickOutside = (event: MouseEvent) => {
+      const target = event.target as HTMLElement;
+      if (target.classList.contains("modal-overlay")) {
+        onClose();
+      }
+    };
+
     document.addEventListener("click", handleClickOutside);
     return () => {
       document.removeEventListener("click", handleClickOutside);
     };
-  }, []);
+  }, [show, onClose]);
+
+  if (!show) {
+    return null;
+  }
 
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center z-50 modal-overlay">
