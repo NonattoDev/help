0f160eab85270de7bc1a0ad813de05638df18980
Nonatto <robsonnonatoiii@gmail.com>
2024-06-29 08:57:32 -0300

    description metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,12 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
+import { metadata } from "./metadata";
 
-export const metadata: Metadata = {
-  title: "Help - Login",
-  icons: {
-    icon: "/help.ico",
-  },
-};
+export { metadata };
 
 export default function RootLayout({
   children,
---
+++ src/app/metadata.ts
@@ -0,0 +1,32 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Help - Login",
+  description:
+    "Transforme a jornada educacional do seu filho com nosso sistema de reforço escolar personalizado. Nossa plataforma inovadora conecta alunos a professores dedicados, proporcionando o apoio necessário para alcançar o sucesso acadêmico de forma eficaz e engajante.",
+  icons: {
+    icon: "/help.ico",
+  },
+  keywords: [
+    "reforço escolar",
+    "sucesso acadêmico",
+    "ensino personalizado",
+    "professores",
+    "alunos",
+    "aulas particulares",
+    "tutoria online",
+    "educação",
+    "plataforma educacional",
+    "apoio escolar",
+    "melhoria de desempenho",
+    "educação personalizada",
+    "mentoria escolar",
+    "professores qualificados",
+    "ensino eficiente",
+  ],
+  authors: [
+    {
+      name: "Lara",
+    },
+  ],
+};
